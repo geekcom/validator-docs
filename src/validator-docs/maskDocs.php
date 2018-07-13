c71aa41e5f7a55516cb84a7619c012f784254592
Allan Denis Morelli Ribeiro <allfamaster@yahoo.com.br>
2018-07-13 09:16:37 -0300

    trait que aplica a mascara e remove a mascara dos attributos listados no docFields

---
+++ src/validator-docs/maskDocs.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Allfa
+ * Date: 13/07/2018
+ * Time: 08:27
+ */
+
+namespace geekcom\ValidatorDocs;
+
+
+trait maskDocs
+{
+    public function getAttribute($key)
+    {
+        $val = parent::getAttribute($key);
+        if (isset($this->docFields) && isset($this->docFields[$key])) {
+            switch ($this->docFields[$key]){
+                case 'cpf':
+                    $val = self::mask($val, '###.###.###-##');
+                    break;
+                case 'cnpj':
+                    $val = self::mask($val, '##.###.###/####-##');
+                    break;
+                case 'cpfcnpj':
+                    $val = self::mask($val, strlen($val) > 11 ? '##.###.###/####-##': '###.###.###-##');
+                    break;
+                default:
+                    throw new \Exception("mascara '$this->docFields[$key]' nao parece ser valida");
+            }
+        }
+        return $val;
+    }
+    public function setAttribute($key, $val){
+        $inner = $val;
+        if(isset($this->docFields) && isset($this->docFields[$key])) {
+            $inner = str_replace([".", "/", "-"], "", $inner);
+        }
+        parent::setAttribute($key, $inner);
+    }
+
+    private function mask($val, $mask)
+    {
+        $maskared = '';
+        $k = 0;
+        for ($i = 0; $i <= strlen($mask) - 1; $i++) {
+            if ($mask[$i] == '#') {
+                if (isset($val[$k]))
+                    $maskared .= $val[$k++];
+            } else {
+                if (isset($mask[$i]))
+                    $maskared .= $mask[$i];
+            }
+        }
+        return $maskared;
+    }
+
+}
